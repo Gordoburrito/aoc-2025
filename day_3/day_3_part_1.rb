9377a5bb7c7d1bd8ac69d1ef23980416a3d8daf9
Dave <qooqu@users.noreply.github.com>
2025-12-10 10:39:25 -0800

    day 3

--- day_3/day_3_part_1.rb
+++ day_3/day_3_part_1.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
-batteries = File.read('day-3-pt1-example.txt').split("\n")
-pp batteries
+# max value with index
+# - exclude the last digit
+# split at index
+# max in second half
+
+def get_batteries
+  # File.read('day-3-pt1-example.txt').split("\n")
+  File.read('day-3.txt').split("\n")
+end
+
+def main
+  batteries = get_batteries
+  tot = 0
+
+  batteries.each do |battery|
+    a = battery.split('').map(&:to_i)
+    a_exclude_last = a[0..-2]
+
+    first_digit = a_exclude_last.max
+    index_of_first = a.index(first_digit)
+
+    a_after_first = a[(index_of_first + 1)..-1]
+    second_digit = a_after_first.max
+
+    ans = first_digit * 10 + second_digit
+    tot += ans
+  end
+
+  tot
+end
+
+pp main
+# ["987654321111111", "811111111111119", "234234234234278", "818181911112111"]
--- day_3/day_3_part_2.rb
+++ day_3/day_3_part_2.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
-batteries = File.read('day-3-pt1-example.txt').split("\n")
-pp batteries
+def get_batteries
+  # File.read('day-3-pt1-example.txt').split("\n")
+  File.read('day-3.txt').split("\n")
+end
+
+def get_stuff(arr, place, accum)
+  arr_exclude_last = arr[0..-place]
+
+  first_digit = arr_exclude_last.max
+  accum << first_digit
+
+  index_of_first = arr.index(first_digit)
+  arr_after_first = arr[(index_of_first + 1)..-1]
+
+  mapped_accum = accum.map(&:to_s).join('').to_i
+  return mapped_accum if place == 1
+
+  get_stuff(arr_after_first, place - 1, accum)
+end
+
+def main
+  batteries = get_batteries
+  tot = 0
+
+  batteries.each do |battery|
+    arr = battery.split('').map(&:to_i)
+    place = 12
+
+    tot += get_stuff(arr, place, [])
+  end
+
+  tot
+end
+
+pp main
