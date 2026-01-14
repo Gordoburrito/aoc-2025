bfb02b611f0d509f9db873a8707ce4f0c4f6c9d0
Gordon Lewis <glewis@berkeley.edu>
2026-01-14 10:46:59 -0800

    Refactor input handling and area calculation in part 1; remove unused input file. Update method names for clarity and enhance area computation logic.

--- day_9/part_1.rb
+++ day_9/part_1.rb
@@ -1,11 +1,38 @@
 # frozen_string_literal: true
 
-def get_stuff
-  File.read('input_example.txt').split("\n")
+def get_tiles
+  File.read('input.txt').split("\n").map{|tile| tile.split(',').map(&:to_i)}
+  # break tiles in arrays
+end
+
+def get_area(pair)
+  corner_1_x, corner_1_y = @tiles[pair[0]]
+  corner_2_x, corner_2_y = @tiles[pair[1]]
+  
+  x_diff = (corner_1_x - corner_2_x).abs + 1
+  y_diff = (corner_1_y - corner_2_y).abs + 1
+
+  return x_diff * y_diff
 end
 
 def main
-  stuff = get_stuff  
+  @tiles = get_tiles
+  tiles_size = @tiles.size
+  max_area = 0
+  
+  pairs = []
+
+  tiles_size.times do |i|
+    (tiles_size - i - 1).times do |j|
+      pairs << [i, j + i + 1]
+    end
+  end
+
+  pairs.each do |pair|
+    max_area = [max_area, get_area(pair)].max
+  end
+
+  return max_area
 end
 
 pp main
--- day_9/part_2.rb
+++ day_9/part_2.rb
@@ -1,1 +1,46 @@
 # frozen_string_literal: true
+
+def get_tiles
+  File.read('input_example.txt').split("\n").map{|tile| tile.split(',').map(&:to_i)}
+  # break tiles in arrays
+end
+
+
+def build_permiter 
+  find adjacent points 
+  { y: [x, x,x]
+  init y 
+end
+
+def max_bounds_perimeter(perimeter)
+  build outside perimeter by min maxing the y column's x's 
+    arr[column] // [min_x, max_x]
+end
+
+def is_valid?
+  check if in max_bounds_perimeter
+  by checking each column if its in bounds of min x and max x 
+end
+
+# def main
+#   @tiles = get_tiles
+#   tiles_size = @tiles.size
+#   max_area = 0
+  
+#   pairs = []
+
+#   tiles_size.times do |i|
+#     (tiles_size - i - 1).times do |j|
+#       pairs << [i, j + i + 1]
+#     end
+#   end
+
+#   pairs.each do |pair|
+#     area = get_area(pair)
+#     max_area = [max_area, area].max if is_valid?
+#   end
+
+#   return max_area
+# end
+
+# pp main
