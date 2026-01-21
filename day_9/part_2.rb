569046da0e07b6902c72e7989ee53c650b7ada8c
Gordon Lewis <glewis@berkeley.edu>
2026-01-21 10:42:45 -0800

    Refactor perimeter calculation in part 2 by replacing build_perimeter with dedicated methods for vertical edge handling and corner validation. Update input file handling and streamline area computation logic for improved clarity and functionality.

--- day_9/part_2.rb
+++ day_9/part_2.rb
@@ -1,115 +1,108 @@
 # frozen_string_literal: true
 
 def get_tiles
-  File.read('input_example.txt').split("\n").map{|tile| tile.split(',').map(&:to_i)}
+  File.read('input.txt').split("\n").map{|tile| tile.split(',').map(&:to_i)}
   # break tiles in arrays
 end
 
 
-def build_perimeter
-  x_edges = {}
-  y_edges = {}
+def get_vertical_edges
+  vertical_edges = {}
   # construct perimeter
   
   @tiles.map do |tile|
-    # find_x_neighbors
-    # make x_edges
-    x_edges[tile[1]] ||= []
-    x_edges[tile[1]] << tile[0]
-    x_edges[tile[1]].sort!
-    # {1=>[7, 11], 7=>[11, 9], 5=>[9, 2], 3=>[2, 7]}
-    
     # find_y_neighbors
-    # make y_edges
-    y_edges[tile[0]] ||= []
-    y_edges[tile[0]] << tile[1]
-    y_edges[tile[0]].sort!
+    # make vertical_edges
+    vertical_edges[tile[0]] ||= []
+    vertical_edges[tile[0]] << tile[1]
+    vertical_edges[tile[0]].sort!
     # {2=>[5, 3], 7=>[1, 3], 9=>[7, 5], 11=>[1, 7]}
   end
-
-
-  def handle_y_edge(prev_x_corner, full_perimeter, x_edges, y_edges)
-    puts prev_x_corner
-    puts y_edges
-    current_y_edge = y_edges[prev_x_corner] # // 11=>[1,7]
-    pp 'current_y_edge'
-    pp current_y_edge
-    y_range = (current_y_edge[0]..current_y_edge[1]).to_a
-    
-    # fill outer perimeter of rows from y_edges
-    y_range.each do |row|
-      full_perimeter[row] ||= []
-      full_perimeter[row] << prev_x_corner
-      full_perimeter[row].sort!
-      pp full_perimeter
-    end
-
-    handle_x_edge(current_y_edge[1], full_perimeter, x_edges, y_edges)
-  end
-
-  def handle_x_edge(prev_y_corner, full_perimeter, x_edges, y_edges)
-    # fill outer perimeter from x_edge
-    
-    # TODO: pass in prev_y_corner = [7, 11]
-    current_x_edge = x_edges[prev_y_corner] # // 7=> [9,11]
-    full_perimeter[prev_y_corner] << current_x_edge[0] # 
-
-    # NOTE: WE NEED TO PASS THE ACTUAL CORNER SO WE KNOW THE DIRECTION WE ARE GOING IN!
-
-    handle_y_edge(current_x_edge[1], full_perimeter)
-  end
-
-  x_edges, y_edges = [x_edges.sort.to_h, y_edges.sort.to_h]
-
-  puts x_edges
-  puts y_edges
-    # //trace the outside. 
-  starting_corner = x_edges[1][1]
-
-  full_perimeter = {}
-  # {1=> [7,11], 2=> [7,11]...}
-  handle_y_edge(starting_corner, full_perimeter, x_edges, y_edges)
-    # or calc within the edges
-  full_perimeter
+  vertical_edges.sort.to_h
 end
 
+def get_opposite_corners
+  tiles_size = @tiles.size
+  opposite_corners = []
 
-#   find adjacent points 
-#   { y: [x, x,x]
-#   init y 
-# end
+  tiles_size.times do |i|
+    (tiles_size - i - 1).times do |j|
+      opposite_corners << [i, j + i + 1]
+    end
+  end
+  
+  opposite_corners.map do |opp_corner|
+    [@tiles[opp_corner[0]], @tiles[opp_corner[1]]]
+  end
 
-# def max_bounds_perimeter(perimeter)
-#   build outside perimeter by min maxing the y column's x's 
-#     arr[column] // [min_x, max_x]
-# end
+end
 
-# def is_valid?
-#   check if in max_bounds_perimeter
-#   by checking each column if its in bounds of min x and max x 
-# end
+def find_intersecting_edges(vertical_edges, row)
+  # select where the row is in between the value range .keys
+  vertical_edges.select do |col, row_span|
+    
+    (row_span[0]..row_span[1]).include?(row)
+  end.keys.minmax
+
+end
+
+def is_valid_corner?(corner, vertical_edges)
+  # corner: [2, 3]
+  # {2=>[3, 5], 7=>[1, 3], 9=>[5, 7], 11=>[1, 7]}
+  row = corner[1]
+  col = corner[0]
+  intersecting_edges = find_intersecting_edges(vertical_edges, row)
+  valid_range = intersecting_edges[0]..intersecting_edges[1]
+  valid_range.include?(col)
+end
+
+def is_valid_group?(corners, vertical_edges)
+  corners.all? do |corner|
+    is_valid_corner?(corner, vertical_edges)
+  end
+end
+
+def get_all_corners(opposite_corner_pair)
+  new_point_1 = [opposite_corner_pair[0][0], opposite_corner_pair[1][1]]
+  new_point_2 = [opposite_corner_pair[1][0], opposite_corner_pair[0][1]]
+  opposite_corner_pair << new_point_1
+  opposite_corner_pair << new_point_2
+end
+
+def get_valid_opposite_corners(opposite_corners, vertical_edges)
+  opposite_corners.select do |opposite_corner_pair|
+    corners = get_all_corners(opposite_corner_pair)
+    is_valid_group?(corners, vertical_edges)
+  end
+end
+
+def get_area(pair)
+  corner_1_x, corner_1_y = pair[0]
+  corner_2_x, corner_2_y = pair[1]
+  
+  x_diff = (corner_1_x - corner_2_x).abs + 1
+  y_diff = (corner_1_y - corner_2_y).abs + 1
+
+  return x_diff * y_diff
+end
+
+def get_max_area(pairs)
+  max_area = 0
+  pairs.each do |pair|
+    max_area = [max_area, get_area(pair)].max
+  end
+  max_area
+end
 
 def main
   @tiles = get_tiles
-  tiles_size = @tiles.size
-  # max_area = 0
-  # pairs = []
 
-  # tiles_size.times do |i|
-  #   (tiles_size - i - 1).times do |j|
-  #     pairs << [i, j + i + 1]
-  #   end
-  # end
-
-  perimeter = build_perimeter
-  # # max_perimeter = max_bounds_perimeter(perimeter)
-
-  # pairs.each do |pair|
-  #   area = get_area(pair)
-  #   max_area = [max_area, area].max if is_valid?(max_perimeter, pairs)
-  # end
-  return perimeter
+  vertical_edges = get_vertical_edges
+  opposite_corners = get_opposite_corners
+  valid_opposite_corners = get_valid_opposite_corners(opposite_corners, vertical_edges)
+  max_area = get_max_area(valid_opposite_corners)
+  return max_area
 end
 
-pp "points of perimeter"
+# pp "rectangles"
 pp main
---
+++ day_9/part_2_old.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+def get_tiles
+  File.read('input_example.txt').split("\n").map{|tile| tile.split(',').map(&:to_i)}
+  # break tiles in arrays
+end
+
+
+def build_perimeter
+  x_edges = {}
+  y_edges = {}
+  # construct perimeter
+  
+  @tiles.map do |tile|
+    # find_x_neighbors
+    # make x_edges
+    x_edges[tile[1]] ||= []
+    x_edges[tile[1]] << tile[0]
+    x_edges[tile[1]].sort!
+    # {1=>[7, 11], 7=>[11, 9], 5=>[9, 2], 3=>[2, 7]}
+    
+    # find_y_neighbors
+    # make y_edges
+    y_edges[tile[0]] ||= []
+    y_edges[tile[0]] << tile[1]
+    y_edges[tile[0]].sort!
+    # {2=>[5, 3], 7=>[1, 3], 9=>[7, 5], 11=>[1, 7]}
+  end
+
+
+  def handle_y_edge(prev_x_corner, full_perimeter, x_edges, y_edges)
+    current_y_edge = y_edges[prev_x_corner] # // 11=>[1,7]
+    get_direction
+    y_range = (current_y_edge[0]..current_y_edge[1]).to_a
+    
+    # fill outer perimeter of rows from y_edges
+    y_range.each do |row|
+      full_perimeter[row] ||= []
+      full_perimeter[row] << prev_x_corner
+      full_perimeter[row].sort!
+      pp full_perimeter
+    end
+
+    handle_x_edge(current_y_edge[1], full_perimeter, x_edges, y_edges)
+  end
+
+  def handle_x_edge(prev_y_corner, full_perimeter, x_edges, y_edges)
+    # fill outer perimeter from x_edge
+    # TODO: pass in prev_y_corner = [7, 11]
+    current_x_edge = x_edges[prev_y_corner] # // 7=> [9,11]
+    full_perimeter[prev_y_corner] << current_x_edge[0] # 
+    # NOTE: WE NEED TO PASS THE ACTUAL CORNER SO WE KNOW THE DIRECTION WE ARE GOING IN!
+    handle_y_edge(current_x_edge[1], full_perimeter)
+  end
+
+  x_edges, y_edges = [x_edges.sort.to_h, y_edges.sort.to_h]
+
+  puts x_edges
+  puts y_edges
+    # //trace the outside. 
+  starting_corner = x_edges[1][1]
+
+  full_perimeter = {}
+  # {1=> [7,11], 2=> [7,11]...}
+  handle_y_edge(starting_corner, full_perimeter, x_edges, y_edges)
+    # or calc within the edges
+  full_perimeter
+end
+
+
+#   find adjacent points 
+#   { y: [x, x,x]
+#   init y 
+# end
+
+# def max_bounds_perimeter(perimeter)
+#   build outside perimeter by min maxing the y column's x's 
+#     arr[column] // [min_x, max_x]
+# end
+
+# def is_valid?
+#   check if in max_bounds_perimeter
+#   by checking each column if its in bounds of min x and max x 
+# end
+
+def main
+  @tiles = get_tiles
+  tiles_size = @tiles.size
+  # max_area = 0
+  # pairs = []
+
+  # tiles_size.times do |i|
+  #   (tiles_size - i - 1).times do |j|
+  #     pairs << [i, j + i + 1]
+  #   end
+  # end
+
+  perimeter = build_perimeter
+  # # max_perimeter = max_bounds_perimeter(perimeter)
+
+  pairs.each do |pair|
+    area = get_area(pair)
+    max_area = [max_area, area].max if is_valid?(max_perimeter, pairs)
+  end
+  return perimeter
+end
+
+pp "points of perimeter"
+pp main
