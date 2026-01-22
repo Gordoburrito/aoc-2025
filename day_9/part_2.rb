a5be737b284cb86a0e4a2f8aca2ea445169a3a4b
Gordon Lewis <glewis@berkeley.edu>
2026-01-22 10:47:46 -0800

    Refactor vertical edge and corner validation logic in part 2. Introduce methods for finding valid ranges and vertical sides, enhancing clarity and functionality. Update main method to utilize new validation structure.

--- day_9/part_2.rb
+++ day_9/part_2.rb
@@ -9,7 +9,6 @@ end
 def get_vertical_edges
   vertical_edges = {}
   # construct perimeter
-  
   @tiles.map do |tile|
     # find_y_neighbors
     # make vertical_edges
@@ -24,7 +23,6 @@ end
 def get_opposite_corners
   tiles_size = @tiles.size
   opposite_corners = []
-
   tiles_size.times do |i|
     (tiles_size - i - 1).times do |j|
       opposite_corners << [i, j + i + 1]
@@ -34,49 +32,60 @@ def get_opposite_corners
   opposite_corners.map do |opp_corner|
     [@tiles[opp_corner[0]], @tiles[opp_corner[1]]]
   end
-
 end
 
 def find_intersecting_edges(vertical_edges, row)
   # select where the row is in between the value range .keys
   vertical_edges.select do |col, row_span|
-    
     (row_span[0]..row_span[1]).include?(row)
   end.keys.minmax
-
 end
 
-def is_valid_corner?(corner, vertical_edges)
-  # corner: [2, 3]
-  # {2=>[3, 5], 7=>[1, 3], 9=>[5, 7], 11=>[1, 7]}
-  row = corner[1]
-  col = corner[0]
-  intersecting_edges = find_intersecting_edges(vertical_edges, row)
-  valid_range = intersecting_edges[0]..intersecting_edges[1]
-  valid_range.include?(col)
+def find_vertical_edges_range(vertical_edges)
+  a, b = vertical_edges.values.flatten.minmax
+  (a..b)
 end
 
-def is_valid_group?(corners, vertical_edges)
-  corners.all? do |corner|
-    is_valid_corner?(corner, vertical_edges)
+def get_valid_ranges(vertical_edges)
+  valid_ranges = {}
+  range_minmax_v_edges = find_vertical_edges_range(vertical_edges)
+  range_minmax_v_edges.each do |row|
+    intersecting_edges = find_intersecting_edges(vertical_edges, row)
+    valid_ranges[row] = intersecting_edges[0]..intersecting_edges[1]
+  end
+  valid_ranges
+end
+
+def is_valid_point?(vertical_sides_point, valid_ranges)
+  row = vertical_sides_point[1]
+  col = vertical_sides_point[0]
+  valid_ranges[row].include?(col)
+end
+
+def is_valid_group?(vertical_sides, valid_ranges)
+  vertical_sides.all? do |vertical_sides_point|
+    is_valid_point?(vertical_sides_point, valid_ranges)
   end
 end
 
-def get_all_corners(opposite_corner_pair)
-  # TODO: get all vertical edge points instead.
-  # to handle the edge case where there is a gap 
-  # we need to get the vertical edges of the box and check
+def get_vertical_sides(opposite_corner_pair)
+  vertical_sides = []
+  side_a_start = opposite_corner_pair[0]
+  side_b_start = opposite_corner_pair[1]
+  side_a_end = [side_a_start[0], side_b_start[1]]
+  y_min, y_max = [side_a_start[1],side_a_end[1]].sort
 
-  new_point_1 = [opposite_corner_pair[0][0], opposite_corner_pair[1][1]]
-  new_point_2 = [opposite_corner_pair[1][0], opposite_corner_pair[0][1]]
-  opposite_corner_pair << new_point_1
-  opposite_corner_pair << new_point_2
+  (y_min..y_max).each do |y|
+    vertical_sides << [side_a_start[0], y]
+    vertical_sides << [side_b_start[0], y]
+  end
+  vertical_sides
 end
 
-def get_valid_opposite_corners(opposite_corners, vertical_edges)
+def get_valid_opposite_corners(opposite_corners, valid_ranges)
   opposite_corners.select do |opposite_corner_pair|
-    corners = get_all_corners(opposite_corner_pair)
-    is_valid_group?(corners, vertical_edges)
+    vertical_sides = get_vertical_sides(opposite_corner_pair)
+    is_valid_group?(vertical_sides, valid_ranges)
   end
 end
 
@@ -102,11 +111,19 @@ def main
   @tiles = get_tiles
 
   vertical_edges = get_vertical_edges
+  valid_ranges = get_valid_ranges(vertical_edges)
+  pp "valid_ranges"
+  pp valid_ranges
   opposite_corners = get_opposite_corners
-  valid_opposite_corners = get_valid_opposite_corners(opposite_corners, vertical_edges)
+  pp "opposite_corner"
+  pp opposite_corners.size
+  valid_opposite_corners = get_valid_opposite_corners(opposite_corners, valid_ranges)
+  pp "valid_opposite_corners"
+  pp valid_opposite_corners.size
   max_area = get_max_area(valid_opposite_corners)
   return max_area
 end
 
-# pp "rectangles"
 pp main
+
+# too low 1289423295
